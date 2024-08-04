e240f0777a3b634e264fac3f6d659095afc9f31d
Hans Yulian <hansyulian@windowslive.com>
2024-08-04 16:02:04 +0700

    models checkpoint

---
+++ lib/models/dashboard_widget_settings_row.dart
@@ -0,0 +1,17 @@
+import 'package:home_dashboard/models/dashboard_widget_settings_column.dart';
+import 'package:home_dashboard/utils/safe_parse_int.dart';
+
+class DashboardWidgetSettingsRow {
+  final List<DashboardWidgetSettingsColumn> columns;
+  final int? flex;
+
+  DashboardWidgetSettingsRow(this.columns, [this.flex]);
+
+  factory DashboardWidgetSettingsRow.fromJson(Map<String, dynamic> json) {
+    var columns = (json['columns'] as List<dynamic>)
+        .map((column) => DashboardWidgetSettingsColumn.fromJson(column))
+        .toList();
+    var flex = safeParseInt(json['flex']);
+    return DashboardWidgetSettingsRow(columns, flex);
+  }
+}
