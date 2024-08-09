749c0f56b46e28a13c7c49ed6fe919655da68a07
Hans Yulian <hansyulian@windowslive.com>
2024-08-09 17:45:41 +0700

    color interpolator

---
+++ lib/modules/color_interpolator.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+class ColorInterpolator {
+  final Color from;
+  final Color to;
+
+  ColorInterpolator(this.from, this.to);
+
+  Color interpolate(double ratio) {
+    // Ensure the ratio is between 0.0 and 1.0
+    ratio = ratio.clamp(0.0, 1.0);
+
+    // Interpolate each color component separately
+    int red = (from.red + (to.red - from.red) * ratio).toInt();
+    int green = (from.green + (to.green - from.green) * ratio).toInt();
+    int blue = (from.blue + (to.blue - from.blue) * ratio).toInt();
+    int alpha = (from.alpha + (to.alpha - from.alpha) * ratio).toInt();
+
+    return Color.fromARGB(alpha, red, green, blue);
+  }
+}
