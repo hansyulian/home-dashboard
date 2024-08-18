89ada8ad8a56adfe20bdace5ba1eeb21d828dfa5
Hans Yulian <10959546+hansyulian@users.noreply.github.com>
2024-08-18 13:42:56 +0700

    full screen (#12)

--- lib/models/dashboard_widget_settings.dart
+++ lib/models/dashboard_widget_settings.dart
@@ -4,25 +4,28 @@ import 'package:home_dashboard/models/time_window.dart';
 class DashboardWidgetSettings {
   final List<DashboardWidgetSettingsRow> rows;
   final TimeWindow? uptime;
+  final bool? startFullscreen;
 
-  DashboardWidgetSettings(this.rows, {this.uptime});
+  DashboardWidgetSettings(this.rows, {this.uptime, this.startFullscreen});
 
   factory DashboardWidgetSettings.fromJson(Map<String, dynamic> json) {
     TimeWindow? uptime =
         json['uptime'] != null ? TimeWindow.fromJson(json['uptime']) : null;
+    bool? startFullscreen = json['startFullscreen'];
     return DashboardWidgetSettings(
-      (json['rows'] as List<dynamic>)
-          .map((row) =>
-              DashboardWidgetSettingsRow.fromJson(row as Map<String, dynamic>))
-          .toList(),
-      uptime: uptime,
-    );
+        (json['rows'] as List<dynamic>)
+            .map((row) => DashboardWidgetSettingsRow.fromJson(
+                row as Map<String, dynamic>))
+            .toList(),
+        uptime: uptime,
+        startFullscreen: startFullscreen);
   }
 
   Map<String, dynamic> toJson() {
     return {
       'rows': rows.map((row) => row.toJson()).toList(),
       'uptimes': uptime?.toJson(),
+      'startFullscreen': startFullscreen,
     };
   }
 }
